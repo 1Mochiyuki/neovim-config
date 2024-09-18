755b1612c8598275f8616e3422a0a4758ce42470
7Yuki <notsmile@proton.me>
2024-09-18 09:09:39 -0400

    using bamboo colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,14 @@
--- return {
---  {
--- Lazy
---    {
---      "rebelot/kanagawa.nvim",
---    },
---  },
---}
-
-return {
-  {
-    "fedepujol/cosmos.nvim",
-    lazy = false,
-  },
+-- Using lazy.nvim
+return -- Using lazy.nvim
+-- Using lazy.nvim
+{
+  "ribru17/bamboo.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("bamboo").setup({
+      -- optional configuration here
+    })
+    require("bamboo").load()
+  end,
 }
