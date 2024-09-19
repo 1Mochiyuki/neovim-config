c3aca064e7781f4e527498af1250fe65769ef167
7Yuki <notsmile@proton.me>
2024-09-19 00:24:14 -0400

    added rose-pine colorscheme setup

--- init.lua
+++ init.lua
@@ -1,9 +1,26 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("mini.surround").setup()
--- vim.cmd("colorscheme bamboo")
-require("bamboo").load()
+require("Comment").setup()
+
+require("rose-pine").setup({
+  variant = "main",
+  dark_variant = "main",
+  extend_background_behind_borders = true,
+  enable = {
+    terminal = true,
+    legacy_highlights = true,
+    migrations = true,
+  },
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = false,
+  },
+})
+vim.cmd("colorscheme rose-pine")
 return {
+
   "kdheepak/lazygit.nvim",
   cmd = {
     "LazyGit",
@@ -20,5 +37,9 @@ return {
   -- order to load the plugin when the command is run for the first time
   keys = {
     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
   },
 }
