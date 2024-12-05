f059a09db1b306614181c282085064c6fe4dedea
7Yuki <notsmile@proton.me>
2024-12-04 19:12:01 -0500

    no longer using lazygit so removed the plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,50 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.keymap.set("n", " e", ":lua MiniFiles.open()<CR>")
+vim.keymap.set("n", "<leader><leader>x", ":source %<cr>")
+
+local neogit = require("neogit")
+
+vim.keymap.set("n", "<leader>gg", function()
+    neogit.open({ kind = "floating" })
+end)
+
+vim.keymap.set("n", "<leader>gc", function()
+    neogit.open({ "commit" })
+end)
+
+vim.keymap.set("n", "<leader>gb", function()
+    neogit.open({ "branch" })
+end)
+
+vim.keymap.set("n", "<leader>gm", function()
+    neogit.open({ "merge" })
+end)
+
+vim.keymap.set("n", "<leader>gr", function()
+    neogit.open({ "remote" })
+end)
+
+vim.keymap.set("n", "<leader>gp", function()
+    neogit.open({ "pull" })
+end)
+
+vim.keymap.set("n", "<leader>gs", function()
+    neogit.open({ "stash" })
+end)
+
+vim.keymap.set("n", "<leader>gw", function()
+    neogit.open({ "worktree" })
+end)
+
+vim.keymap.set("n", "<leader>grb", function()
+    neogit.open({ "rebase" })
+end)
+
+vim.keymap.set("n", "<leader>gi", function()
+    neogit.open({ "ignore" })
+end)
+
+vim.keymap.set("n", "<leader>gf", function()
+    neogit.open({ "log" })
+end)
