7c20902b68fff9cd038cf63c5bd37c6a9727f476
7Yuki <notsmile@proton.me>
2024-12-18 16:02:37 -0500

    disabled tokyonight theme

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
     { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    { "folke/tokyonight.nvim", enabled = false },
     { "hrsh7th/nvim-cmp", enabled = false },
 }
