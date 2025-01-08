6798aa913d68f42259d5ddd2ab895b38bae8e921
7Yuki <notsmile@proton.me>
2025-01-08 14:51:22 -0500

    disabled some plugins from snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+    "snacks.nvim",
+    opts = {
+        scroll = { enabled = false },
+        lazygit = { enabled = false },
+        statuscolumn = { enabled = false },
+        scope = { enabled = false },
+        dim = { enabled = false },
+    },
+}
