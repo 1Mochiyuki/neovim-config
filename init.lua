939f7475faf0410c15284021c1420d776bf1141b
1Mochiyuki <notsmile@protonmail.com>
2025-01-16 22:29:34 -0500

    uses ripgrep by default

--- init.lua
+++ init.lua
@@ -6,6 +6,12 @@ require("mini.files").setup({
     },
 })
 
+-- uses ripgrep instead of regular grep
+vim.o.grepprg = "rg --vimgrep --smart-case --hidden"
+vim.o.grepformat = "%f:%l:%c:%m"
+-- sets <search query>/g to be on by default, add /g to end of search query to undo this
+vim.o.gdefault = true
+
 if jit.os == "Windows" then
     vim.o.shellcmdflag =
         "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
@@ -22,5 +28,5 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.scrolloff = 12
+vim.opt.scrolloff = 4
 vim.opt.mouse = ""
